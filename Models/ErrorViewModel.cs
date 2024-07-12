18dd598768200682e60aff119f33f2b7eb5bbf39
ShiZhaoyang <shizhywin@qq.com>
2024-07-12 13:15:59 -0700

    Init project!

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Vangoph.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace Vangoph.Models;
+
+public class Image
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime CreationDate { get; set; }
+    public string ImageUrl { get; set; }
+    public double Price { get; set; }
+    public int AuthorId { get; set; }
+}
