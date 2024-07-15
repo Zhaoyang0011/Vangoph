96dfd2994d8f8d68ff9d469de190d4eadd9adec3
jasonwillhudson <38706702+jasonwillhudson@users.noreply.github.com>
2024-07-14 17:16:56 -0700

    upload image style completed

--- obj/Debug/net8.0/Vangoph.AssemblyInfo.cs
+++ obj/Debug/net8.0/Vangoph.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vangoph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a796fcb907c3016f2bc512b84ea80e97ef57b5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe64e49e08ee256a8a67981fe5d0e9e25743232e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vangoph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vangoph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//limit form input to 'count' decimal
+function limitDecimalPlaces(e, count) {
+  if (e.target.value.indexOf(".") == -1) {
+    return;
+  }
+  if (e.target.value.length - e.target.value.indexOf(".") > count) {
+    e.target.value = parseFloat(e.target.value).toFixed(count);
+  }
+}
+
