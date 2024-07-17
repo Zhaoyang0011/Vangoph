6dd10b028d564cdfff510143581464b60f07ce1b
ShiZhaoyang <shizhywin@qq.com>
2024-07-17 03:37:58 -0700

    Implement email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,16 +2,20 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Vangoph.Models;
+using Vangoph.Reqeusts;
+using Vangoph.Services;
 
 namespace Vangoph.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly MailSender _mailSender;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MailSender mailSender)
     {
         _logger = logger;
+        _mailSender = mailSender;
     }
 
     public IActionResult Index()
@@ -24,9 +28,21 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpPost("/mail")]
+    public async Task<IActionResult> SendEmail([FromForm] ContactRequest request)
+    {
+        if (ModelState.IsValid)
+        {
+            await _mailSender.SendAsync(request.Email, "Question", request.Description);
+            return Redirect("Image");
+        }
+
+        return View("Error", "Attribute Invalid!");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View("/Error","Unknow Error");
+        return View("/Error", "Unknow Error");
     }
 }
---
+++ Reqeusts/ContactRequest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vangoph.Reqeusts;
+
+public class ContactRequest
+{
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required")]
+    [Phone]
+    public string Phone { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    public string Description { get; set; }
+}
