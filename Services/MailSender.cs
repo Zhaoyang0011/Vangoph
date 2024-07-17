68f167d6b62899afdb13b80fa45cf2d059b9a54a
ShiZhaoyang <shizhywin@qq.com>
2024-07-16 17:01:01 -0700

    Add mail sender

---
+++ Services/MailSender.cs
@@ -0,0 +1,28 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+
+namespace Vangoph.Services;
+
+public class MailSender
+{
+    public async Task SendAsync(string to, string subject, string text)
+    {
+        // create message
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("vangoph.6844@hotmail.com"));
+        email.To.Add(MailboxAddress.Parse(to));
+        email.Subject = subject;
+        email.Body = new TextPart(TextFormat.Plain) { Text = text };
+
+        // send email
+        using (var smtp = new SmtpClient())
+        {
+            await smtp.ConnectAsync("smtp.office365.com", 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync("vangoph.6844@hotmail.com", "Vangoph_6844");
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
